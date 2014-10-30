7955dfc478d0b6dc6b179e737c33294db2d719c5
Nikolas Huth <nch2116@columbia.edu>
2014-10-30 13:57:49 +0000

    work

--- FactorMath.java
+++ FactorMath.java
@@ -8,6 +8,17 @@ public class FactorMath {
 		System.out.println("This will factor out whatever ")
 		System.out.println("Please input in the form: ax^2 + bx + c")
 		rawInput = scan.nextLine();
-		if((!(rawInput.contains("x"))) ||  )
+		rawInput = rawInput.replaceAll("\\s+","");
+		if(rawInput.contains("x") && rawInput.contains("^2") {
+			String a; String b; String c;
+			a = rawInput.substring(0, 1); 
+			b = rawInput.substring();
+			c = rawInput.substring()
+		
+		}
+		else {
+		System.out.println("Please input in the form ax^2 + bx + c");
+		main();
+		}
 	}
 }
