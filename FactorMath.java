4954fd178ca4f76030770b1748c5f1e606d4fb1f
Nikolas Huth <nch2116@columbia.edu>
2014-10-30 12:30:31 +0000

    stuff

---
+++ FactorMath.java
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+public class FactorMath {
+
+	public static void main(Strin[] args) {
+		Scanner scan = new Scanner(System.in);
+		String rawInput;
+		System.out.println("This will factor out whatever ")
+		System.out.println("Please input in the form: ax^2 + bx + c")
+		rawInput = scan.nextLine();
+		if((!(rawInput.contains("x"))) ||  )
+	}
+}
