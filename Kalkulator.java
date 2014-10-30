8f437e73f02aaa5cd2ca5b590ce9bf6d334ebdeb
Rep Woroch <rep_woroch@asl.org>
2014-10-30 12:13:36 +0000

    Updated the Kalkulator Class.
    Added messages and prompts asking what they would like to do.

--- Kalkulator.java
+++ Kalkulator.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Kalkulator {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Kalkulator. Here you can perform complex math equations.");
+		System.out.println("What type of calculation would you like to do?");
+		System.out.println("Choose from the following options:");
+		System.out.println("Factoring");
+		System.out.println("Algebra");
+		System.out.println("Basic");
+		
+		Scanner scnr = new Scanner(System.in);
+		String input = scnr.next().toLowerCase();
+		
+		if (input.equals("factoring")) {
+			//User wants to do a factoring equation
+			//Probably call the FactorMath class
+		}
+		
+		else if (input.equals("algebra")) {
+			//User wants to do a factoring equation
+			//Probably call the AlgebraMath class
+		}
+		
+		else if (input.equals("basic")) {
+			//User wants to do a factoring equation
+			//Probably call the BasicMath class
+		}
+		
+		else {
+			System.out.println("Sorry for wasting your time.");
+			return;
+		}
+		
 	}
 }
