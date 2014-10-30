f47088f52ecbb25e25a26b34c476c4b8072b83a4
Rep Woroch <rep_woroch@asl.org>
2014-10-30 12:24:27 +0000

    Updated Kalkulator class to call other classes.

--- Kalkulator.java
+++ Kalkulator.java
@@ -15,22 +15,30 @@ public class Kalkulator {
 		if (input.equals("factoring")) {
 			//User wants to do a factoring equation
 			//Probably call the FactorMath class
+			
+			FactorMath factoring = new FactorMath();
+			factoring.main(String[] args);	
 		}
 		
 		else if (input.equals("algebra")) {
 			//User wants to do a factoring equation
 			//Probably call the AlgebraMath class
+			
+			AlgebraMath algebra = new AlgebraMath();
+			algebra.main(String[] args);
 		}
 		
 		else if (input.equals("basic")) {
 			//User wants to do a factoring equation
 			//Probably call the BasicMath class
+			
+			BasicMath basic = new BasicMath();
+			basic.main(String[] args);
 		}
 		
 		else {
 			System.out.println("Sorry for wasting your time.");
 			return;
 		}
-		
 	}
 }
